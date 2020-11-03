c148a31d9ffa8a50a65343c36c880e823582227e
Victor Jimenez <vtorero@gmail.com>
2020-11-03 00:25:22 -0500

    labs

--- Laundry/ConexBD.cs
+++ Laundry/ConexBD.cs
@@ -18,7 +18,7 @@ namespace Lavanderia
             {
                 //string enlace ="Server=kvconsult.com; Port=3306 ;Database=kvconsul_lavan; uid=kvconsul_lavan; pwd=vji2002;";
               //string enlace = "Server=107.190.129.66; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=cualesmi_web; pwd=vji2002;";
-                string enlace = "Server=35.231.78.51; Port=3306 ;Database=dashboard; uid=marife; pwd=libido16;";
+                string enlace = "Server=35.231.78.51; Port=3306 ;Database=dashboard2; uid=marife; pwd=libido16;";
                //string enlace = "Server=localhost; Port=3306 ;Database=cualesmi_lavanderia; uid=root; pwd=;";
                 this.con = new MySqlConnection(enlace);
                 this.con.Open();
